41928f3eb75a23fe95bc8db9b54079bfdfda2fa9
Aaron <aaron1@metschnet.com>
2022-08-03 16:12:26 -0400

    auto resolution and fullscreen toggle

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import glob
 import random
+import hashlib
+
+import pyautogui
 
 import pygame
 from pygame.locals import *
@@ -44,6 +47,8 @@ edges = (
 #     (0, -.05, .05)
 # )
 
+width, height = pyautogui.size()
+
 sphere = gluNewQuadric()
 
 def Cube():
@@ -57,9 +62,10 @@ def Cube():
 
 def main():
     pygame.init()
-    display = (1920, 1080)
-    pygame.display.set_caption('PyOpenGL X pygame test')
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL | pygame.FULLSCREEN)
+    display = (width / 2 , height / 2)
+    pygame.display.set_caption('PyOpenGL X pygame test', 'sclogo.png')
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
     pygame.mixer.init()
     
     pygame.mixer.music.set_volume(.9)
@@ -69,21 +75,22 @@ def main():
         li.append(i)
     for i in glob.glob('./music/*.wav'):
         li.append(i)
-    random.seed(datetime.now().microsecond)
+    now = datetime.now()
+    random.seed(hashlib.sha256(now.__str__().encode('UTF-8')).hexdigest())
     random.shuffle(li)
 
-    #print(li[0])
+    print(li[0])
     pygame.mixer.music.load(li.pop(0))
     pygame.mixer.music.play()
-
-
-    for song in li:
-        #print(song)
-        pygame.mixer.music.queue(li.pop(li.index(song)))
+    print(li[0])
+    pygame.mixer.music.queue(li.pop(0))
+    pygame.mixer.music.set_endevent(765045)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     glTranslatef(0.0, 0.0, -5)
+
+    sph = True
 
     while True:
         for event in pygame.event.get():
@@ -91,6 +98,9 @@ def main():
                 pygame.mixer.quit()
                 pygame.quit()
                 exit(0)
+            if event.type == pygame.mixer.music.get_endevent():
+                print(li[0])
+                pygame.mixer.music.queue(li.pop(0))
             if event.type == pygame.KEYDOWN:
                 keys = pygame.key.get_pressed()
                 if keys[K_q] or keys[K_x] or keys[K_ESCAPE]:
@@ -103,15 +113,20 @@ def main():
                     pygame.mixer.music.unpause()
                 if keys[K_r]:
                     pygame.mixer.music.rewind()
-
+                if keys[K_f] or keys[K_F11]:
+                    pygame.display.toggle_fullscreen()
+                if keys[K_BACKSLASH]:
+                    random.shuffle(li)
+                if keys[K_h]:
+                    sph = not sph
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         Cube()
         glColor4f(0x00, 0xff, 0xff, 1)
-        gluSphere(sphere, 0.069, 64, 64)
+        if sph:
+            gluSphere(sphere, 0.069, 64, 64)
         pygame.display.flip()
         pygame.time.wait(10)
-
 
 main()
